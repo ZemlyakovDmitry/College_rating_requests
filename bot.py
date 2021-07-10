bd19a845f2fee42e42fc699adb09fd0403aaf58b
Dmitriy Zemliakov <zacon486@gmail.com>
2021-07-10 14:53:05 +0300

    added timeouts on requests

--- bot.py
+++ bot.py
@@ -4,27 +4,27 @@ from lxml import html
 import requests
 import config as cfg
 
-previous = 11
+previous = 1
 headers ={
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81',
 }
 
 def bot():
     global previous
-    page = requests.get(cfg.url_rating, headers=headers)
+    page = requests.get(cfg.url_rating, headers=headers, timeout=10)
     tree = html.fromstring(page.content)
-    rating = tree.xpath("//td[contains(text(),'Земляков')]/preceding-sibling::td[1]/text()")
+    rating = tree.xpath("//td[contains(text(),'SURNAME')]/preceding-sibling::td[1]/text()")  # Place your surname instead of the 'SURNAME'
     rate = int(str(rating)[2:-2])
     diff = rate - previous
     if diff == 0:
         data = {'chat_id': cfg.chat_id, 'text': 'С прошедшего дня ничего не изменилось. Текущее место в рейтинге абитуриентов: ' + str(rate)}
-        r = requests.post(cfg.url, data=data)
+        r = requests.post(cfg.url, data=data, timeout=10)
     if diff < 0:
         data = {'chat_id': cfg.chat_id, 'text': 'Кто-то забрал заявление. Текущее место в рейтинге абитуриентов: ' + str(rate) + ' (+' + str(abs(diff)) + ')'}
-        r = requests.post(cfg.url, data=data)
+        r = requests.post(cfg.url, data=data, timeout=10)
     if diff > 0:
         data = {'chat_id': cfg.chat_id, 'text': 'Погружение. Текущее место в рейтинге абитуриентов: ' + str(rate) + ' (-' + str(diff) + ')'}
-        r = requests.post(cfg.url, data=data)
+        r = requests.post(cfg.url, data=data, timeout=10)
     previous = rate
     Timer(43200, bot).start()  # Waiting 12 hours
     
